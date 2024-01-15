215154a5ee56e0a51752090254942cb1f830d553
Luis <luis.marquez.mt@usa.edu.co>
2024-01-15 16:56:53 -0500

    commit segundo

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <p className="text-orange-300 overline">Dashboard PagE ome</p>;
+  }
---
+++ app/dashboard/parceurs/page.tsx
@@ -0,0 +1,6 @@
+import { lusitana } from '@/app/ui/fonts'
+
+
+export default function Page() {
+    return <p className={`${lusitana.className} text-3xl capitalize text-gray-800` }>PArceurs Page</p>;
+  }
